28ea6e3fa449d83fee802315bf748579f63112d7
hiepht <091220>
2023-01-30 17:00:34 +0700

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,15 @@
+import React, { Children } from 'react'
+import Header from './Header'
+
+
+
+const Layout = ({children}) => {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  )
+}
+
+export default Layout
